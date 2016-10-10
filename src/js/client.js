432deeec9085b435bd8a24efdf50d255ea3f1c97
aleksandarcrvc <aleksandarcrvc@gmail.com>
2016-10-10 16:31:14 +0200

    adds links for loading samle videos

--- src/js/client.js
+++ src/js/client.js
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 import Player from "./components/player";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Player source={'../../assets/videos/SampleVideo_1280x720_2mb.mp4'} />, app);
+ReactDOM.render(<Player />, app);
--- src/js/components/player.js
+++ src/js/components/player.js
@@ -2,12 +2,23 @@ import React from "react";
 
 export default class Player extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      source: '../../assets/videos/sample.mp4'
+    };
+  }
+  onClickHandler(event){
+    let videoSource = event.target.getAttribute('data-video')
+    this.setState({ source: videoSource });
+  }
   render() {
-    const { source } = this.props; 
     return (
       <div>
-        <video src={source} width="100%" controls>
+        <video src={this.state.source} width="100%" height="600px"controls>
         </video>
+        <a href='javascript:void(0)' data-video="../../assets/videos/sample.mp4" onClick={this.onClickHandler.bind(this)}>Video 1</a> | &nbsp;&nbsp; 
+        <a href='javascript:void(0)' data-video="../../assets/videos/gummy-bear.mp4" onClick={this.onClickHandler.bind(this)}>Video 2</a>       
       </div>
     );
   }
